4c5c4c54bdf30cbdd030df2ec521b7e529b934ff
Digibori-Laptop <digiborimusik@gmail.com>
2021-03-10 20:52:38 +0200

    chore

--- lib/insertionSorting.dart
+++ lib/insertionSorting.dart
@@ -1,12 +1,12 @@
 List insertionSort(List arr) {
   for (var i = 1; i < arr.length; i++) {
     var key = arr[i];
-    var shit = i - 1;
-    while (shit >= 0 && arr[shit] > key) {
-      arr[shit + 1] = arr[shit];
+    var shit = i;
+    while (shit > 0 && arr[shit - 1] > key) {
+      arr[shit] = arr[shit - 1];
       shit--;
     }
-    arr[shit + 1] = key;
+    arr[shit] = key;
   }
   return arr;
 }
